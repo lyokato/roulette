2ec8259456b95ae6cbcd1cb556de0a70d48bbd99
lyokato <lyo.kato@gmail.com>
2018-03-14 07:51:05 +0000

    version 0.2.5

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Roulette.Mixfile do
   def project do
     [
       app: :roulette,
-      version: "0.2.4",
+      version: "0.2.5",
       elixir: "~> 1.5",
       package: package(),
       start_permanent: Mix.env == :prod,
