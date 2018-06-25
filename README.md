825803edd3c5e5f2718ba8630771a0727af1ee86
lyokato <lyo.kato@gmail.com>
2018-06-25 04:57:21 +0000

    ver 1.0.1

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ by adding `roulette` to your list of dependencies in `mix.exs`:
 ```elixir
 def deps do
   [
-    {:roulette, "~> 1.0.0"}
+    {:roulette, "~> 1.0.1"}
   ]
 end
 ```
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Roulette.Mixfile do
   def project do
     [
       app: :roulette,
-      version: "1.0.0",
+      version: "1.0.1",
       elixir: "~> 1.6",
       package: package(),
       start_permanent: Mix.env == :prod,
